bce5516ee7ceeadecbce1ca2e0757e80aeebc787
Orffen <orffen@orffenspace.com>
2020-05-01 21:33:16 +1000

    Update pseudoHex to accept either string or number

--- tools/pseudohex.js
+++ tools/pseudohex.js
@@ -1,4 +1,10 @@
-function pseudoHex(number) {
+function pseudoHex(value) {
     const hex = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    return hex[number];
+    if (typeof value === 'number') {
+        return hex[value];
+    } else if (typeof value === 'string') {
+        return hex.indexOf(value);
+    } else {
+        console.error("Can only convert int and str types to pseudohex!");
+    }
 }
